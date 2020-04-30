a0fd4bc826bceac1c103490af0d86984aeeab0d2
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2020-04-30 17:10:55 +0200

    setup rollup builder and added uikit css framework to it

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,16 @@
 <script>
-	export let name;
+	import "node_modules/uikit/dist/css/uikit.min.css"
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-left">
+        <ul class="uk-navbar-nav">
+            <li class="uk-active"><a href="www.google.se">Test</a></li>
+            <li class="uk-parent"><a href="www.google.se">test2</a></li>
+            <li><a href="www.google.se">Testi</a></li>
+        </ul>
+    </div>
+</nav>
