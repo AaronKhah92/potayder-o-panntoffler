c45a299e62dc66dc4c483145990bc2a6e7011f53
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2020-05-08 06:07:43 +0200

    Added componenets like a audioplayer to add to pages

---
+++ src/components/audiolinks.svelte
@@ -0,0 +1,123 @@
+<script>
+        //Skane
+        //export let bara;        
+/*         export let bjuv;        
+        export let broby;        
+        export let loderup;        
+        export let norrarorom;        
+        export let osssjo;        
+        
+        
+        export let hallevik;        
+        export let jamshog;        
+        export let torhamn;
+        
+        export let ankarsrum;
+        export let torsas;
+        
+        export let lanna;
+        export let sorunda;
+        export let vingaker; */
+          
+
+ 
+
+    
+       /*  
+               oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/bara/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/bara/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/bara/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/bara/youngwoman.mp3"
+       
+       oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/bjuv/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/bjuv/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/bjuv/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/bjuv/youngwoman.mp3"
+    
+
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/broby/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/broby/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/broby/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/broby/youngwoman.mp3"
+  
+  
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/loderup/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/loderup/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/loderup/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/loderup/youngwoman.mp3"
+     
+  
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/norrarorom/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/norrarorom/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/norrarorom/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/norrarorom/youngwoman.mp3"
+     
+  
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/osssjo/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/osssjo/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/osssjo/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/osssjo/youngwoman.mp3"
+     
+  
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/hallevik/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/hallevik/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/hallevik/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/hallevik/youngwoman.mp3"
+     
+   
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/jamshog/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/jamshog/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/jamshog/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/jamshog/youngwoman.mp3"
+     
+   
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/torhamn/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/torhamn/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/torhamn/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/torhamn/youngwoman.mp3"
+    
+   
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/ankarsrum/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/ankarsrum/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/ankarsrum/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/ankarsrum/youngwoman.mp3"
+     
+   
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/torsas/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/torsas/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/torsas/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/torsas/youngwoman.mp3"
+     
+ 
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/lanna/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/lanna/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/lanna/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/lanna/youngwoman.mp3"
+   
+  
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/sorunda/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/sorunda/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/sorunda/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/sorunda/youngwoman.mp3"
+  
+  
+        oldman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/vingaker/oldman.mp3",
+        oldwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/vingaker/oldwoman.mp3",
+        youngman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/vingaker/youngman.mp3",
+        youngwoman: "https://aaronkhah92.github.io/potayder-o-panntoffler/public/assets/counties/skane/vingaker/youngwoman.mp3"
+   
+ */
+</script>
+
+<!--  	{#each bara as bar, i}
+  <audio id="audio" controls preload="auto">
+			<source src={bar[oldman]} type="audio/mpeg">
+	</audio>
+	{/each} -->
+
+<!--   <p>
+	The <code>{name}</code> package is {speed} fast.
+	Download version {version} from <a href="https://www.npmjs.com/package/{name}">npm</a>
+	and <a href={website}>learn more here</a>
+</p> -->
+
---
+++ src/components/modal.svelte
@@ -0,0 +1,22 @@
+<script>
+
+
+export let modalTitle;
+export let modalId;
+</script>
+
+
+<div id={modalId} uk-modal>
+  <div class="uk-modal-dialog">
+    <button class="uk-modal-close-default" type="button" uk-close></button>
+    <div class="uk-modal-header">
+      <h2 class="uk-modal-title">{modalTitle}</h2>
+    </div>
+    <div class="uk-modal-body">
+          <slot></slot>
+    </div>
+    <div class="uk-modal-footer uk-text-right">
+      <button class="uk-button uk-button-default uk-modal-close" type="button">Stäng</button>
+    </div>
+  </div>
+</div>
