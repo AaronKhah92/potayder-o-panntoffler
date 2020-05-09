5e91fdc9449b23cb4c41d411de5312476be1a7fa
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2020-05-09 23:23:59 +0200

    component name chane and styling change

---
+++ src/components/changebutton.svelte
@@ -0,0 +1,18 @@
+<script>
+
+export let targetCityId;
+export let targetCityName;
+</script>
+
+<button class="uk-button-default blue-btn" type="button" uk-toggle="target: {targetCityId}">Ändra kön {targetCityName}</button>
+
+
+<style>
+
+.blue-btn {
+  background-color: #C6ECFF;
+  padding: 12px 12px;
+  margin: 5px 15px
+}
+
+</style>
