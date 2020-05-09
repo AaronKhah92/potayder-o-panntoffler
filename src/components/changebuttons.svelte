5e19761ba35efad27544e9017688ac8334627095
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2020-05-09 20:05:41 +0200

    added a button component to switch between man and woman voices

---
+++ src/components/changebuttons.svelte
@@ -0,0 +1,17 @@
+<script>
+
+export let targetCityId;
+
+</script>
+
+<button class="uk-button-default red-btn" type="button" uk-toggle="target: {targetCityId}">Ändra kön</button>
+
+
+<style>
+
+.red-btn {
+  background-color: #FFB69F;
+  padding: 15px 15px;
+}
+
+</style>
